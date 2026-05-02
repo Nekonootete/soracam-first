330541c7d30d8f25af2495f7bae31deeef1363a4
y-egashira <160115155+Nekonootete@users.noreply.github.com>
2026-05-03 00:13:12 +0900

    Initial commit

---
+++ test/soracam-first.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib/core';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as SoracamFirst from '../lib/soracam-first-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/soracam-first-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new SoracamFirst.SoracamFirstStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
